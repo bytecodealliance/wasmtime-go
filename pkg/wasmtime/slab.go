7b478d095bf93cb09aa4d47b7b23b25abfb6fef5
Alex Crichton <alex@alexcrichton.com>
2020-03-30 20:32:34 -0700

    gofmt

--- pkg/wasmtime/slab.go
+++ pkg/wasmtime/slab.go
@@ -1,20 +1,20 @@
 package wasmtime
 
 type slab struct {
-  list []int
-  next int
+	list []int
+	next int
 }
 
 func (s *slab) allocate() int {
-  if s.next == len(s.list) {
-    s.list = append(s.list, s.next)
-  }
-  ret := s.next
-  s.next = s.list[ret]
-  return ret
+	if s.next == len(s.list) {
+		s.list = append(s.list, s.next)
+	}
+	ret := s.next
+	s.next = s.list[ret]
+	return ret
 }
 
 func (s *slab) deallocate(slot int) {
-  s.list[slot] = s.next
-  s.next = slot
+	s.list[slot] = s.next
+	s.next = slot
 }
