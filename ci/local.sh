dc50b7ee82dc896b9f981b8f81819adb0379cb70
Olivier Lemasle <o.lemasle@gmail.com>
2021-06-03 21:12:41 +0200

    Add build/include/wasmtime
    Following the update to Wasmtime's new C API in #81, the header files are now not
    only in build/local, but also in build/local/wasmtime.
    
    This commit adds the directory "build/include/wasmtime" as a valid empty go
    package, like it was done in #45 for the other build directories.
    
    It also addapts `ci/local.sh` to:
    - copy the additional header files in build/include/wasmtime
    - stop this script from removing the "empty.go" files.

--- ci/local.sh
+++ ci/local.sh
@@ -6,19 +6,27 @@ if [ "$wasmtime" = "" ]; then
   exit 1
 fi
 
+# Clean and re-create "build" directory hierarchy
 rm -rf build
+for d in "include" "include/wasmtime" "linux-x86_64" "macos-x86_64" "windows-x86_64"; do
+  mkdir -p "build/$d"
+  name=$(basename $d)
+  echo "package ${name/-/_}" > "build/$d/empty.go"
+done
 
-build=$wasmtime/target/release
-if [ ! -d $build ]; then
-  build=$wasmtime/target/debug
+build="$wasmtime/target/release"
+if [ ! -d "$build" ]; then
+  build="$wasmtime/target/debug"
 fi
+# Use absolute path for symbolic links
+build=$(readlink -f "$build")
 
-if [ ! -f $build/libwasmtime.a ]; then
+if [ ! -f "$build/libwasmtime.a" ]; then
   echo 'Missing libwasmtime.a. Did you `cargo build -p wasmtime-c-api`?'
 fi
 
-mkdir -p build/{include,linux-x86_64,macos-x86_64}
-ln -s $build/libwasmtime.a build/linux-x86_64/libwasmtime.a
-ln -s $build/libwasmtime.a build/macos-x86_64/libwasmtime.a
-cp $wasmtime/crates/c-api/include/*.h build/include
-cp $wasmtime/crates/c-api/wasm-c-api/include/*.h build/include
+ln -s "$build/libwasmtime.a" build/linux-x86_64/libwasmtime.a
+ln -s "$build/libwasmtime.a" build/macos-x86_64/libwasmtime.a
+cp "$wasmtime"/crates/c-api/include/*.h build/include
+cp -r "$wasmtime"/crates/c-api/include/wasmtime build/include
+cp "$wasmtime"/crates/c-api/wasm-c-api/include/*.h build/include
