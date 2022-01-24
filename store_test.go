ecf7e9bac48c6efe069b3c409435ff9bac46994d
Yang Hau <vulxj0j8j8@gmail.com>
2022-01-25 01:06:42 +0800

    store: Add Bindings for fuel relating functions (#117)
    * store: Add FuelConsumed and SetConsumFuel
    
    * store: Add AddFuel and ConsumeFuel
    
    * store: Add test for AddFuel and ConsumeFuel
    
    * store: Add documents

--- store_test.go
+++ store_test.go
@@ -64,3 +64,65 @@ func TestInterruptWasm(t *testing.T) {
 		panic("expected a trap")
 	}
 }
+
+func TestFuelConsumed(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+
+	fuel, enable := store.FuelConsumed()
+	if enable != false {
+		t.Fatal("expected not enable")
+	}
+	if fuel != 0 {
+		t.Fatalf("fuel is %d, not zero", fuel)
+	}
+}
+
+func TestAddFuel(t *testing.T) {
+	config := NewConfig()
+	config.SetConsumeFuel(true)
+	engine := NewEngineWithConfig(config)
+	store := NewStore(engine)
+
+	fuel, enable := store.FuelConsumed()
+	if enable != true {
+		t.Fatal("expected enabled")
+	}
+	if fuel != 0 {
+		t.Fatalf("fuel is %d, not zero", fuel)
+	}
+
+	const add_fuel = 3
+	if err := store.AddFuel(add_fuel); err != nil {
+		t.Fatal("expected no error")
+	}
+}
+
+func TestConsumeFuel(t *testing.T) {
+	config := NewConfig()
+	config.SetConsumeFuel(true)
+	engine := NewEngineWithConfig(config)
+	store := NewStore(engine)
+
+	fuel, enable := store.FuelConsumed()
+	if enable != true {
+		t.Fatal("expected enabled")
+	}
+	if fuel != 0 {
+		t.Fatalf("fuel is %d, not zero", fuel)
+	}
+
+	const add_fuel = 3
+	if err := store.AddFuel(add_fuel); err != nil {
+		t.Fatal("expected no error")
+	}
+
+	consume_fuel := uint64(1)
+	remaining, err := store.ConsumeFuel(consume_fuel)
+	if err != nil {
+		t.Fatal("expected no error")
+	}
+	if remaining != (add_fuel - consume_fuel) {
+		t.Fatalf("expected %d, but %d", add_fuel-consume_fuel, remaining)
+	}
+}
