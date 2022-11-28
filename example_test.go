894cfc091290c97f744b429a44e2d6ad8009d634
Alex Crichton <alex@alexcrichton.com>
2022-11-28 10:05:49 -0600

    Add `v3` to module string (#161)
    * Add `v3` to module string
    
    Attempt to handle #157
    
    * Try to fix bazel

--- example_test.go
+++ example_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bytecodealliance/wasmtime-go"
+	"github.com/bytecodealliance/wasmtime-go/v3"
 )
 
 // Example of limiting a WebAssembly function's runtime using "fuel consumption".
