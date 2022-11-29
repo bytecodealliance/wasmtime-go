0918e5d9394e7d8fc2952a08e9142aa31bdd2851
Alex Crichton <alex@alexcrichton.com>
2022-11-29 07:21:22 -0800

    Update more include paths

--- includebuild.go
+++ includebuild.go
@@ -12,11 +12,11 @@ import (
 	// included in vendored dependencies.
 	// Cf. https://github.com/golang/go/issues/26366
 
-	_ "github.com/bytecodealliance/wasmtime-go/build/include"
-	_ "github.com/bytecodealliance/wasmtime-go/build/include/wasmtime"
-	_ "github.com/bytecodealliance/wasmtime-go/build/linux-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/build/linux-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/build/macos-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/build/macos-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/build/windows-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/include"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/include/wasmtime"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/linux-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/linux-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/macos-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/macos-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v3/build/windows-x86_64"
 )
