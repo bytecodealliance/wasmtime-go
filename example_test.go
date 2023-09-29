c0503351a77e9a9a44b71cfe22eb456cc747d9e1
Alex Crichton <alex@alexcrichton.com>
2023-09-29 09:15:32 -0500

    Fix example test syntax (#196)

--- example_test.go
+++ example_test.go
@@ -30,7 +30,7 @@ func ExampleConfig_fuel() {
 	  (func $fibonacci (param $n i32) (result i32)
 	    (if
 	      (i32.lt_s (local.get $n) (i32.const 2))
-	      (return (local.get $n))
+	      (then (return (local.get $n)))
 	    )
 	    (i32.add
 	      (call $fibonacci (i32.sub (local.get $n) (i32.const 1)))
