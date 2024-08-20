cb7232c1a3761430bc730efc16a9c76090e1c1aa
Alex Crichton <alex@alexcrichton.com>
2024-08-20 09:40:40 -0700

    Bump to 24.0.0

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/bytecodealliance/wasmtime-go/v23
+module github.com/bytecodealliance/wasmtime-go/v24
 
 go 1.18
 
--- includebuild.go
+++ includebuild.go
@@ -12,11 +12,11 @@ import (
 	// included in vendored dependencies.
 	// Cf. https://github.com/golang/go/issues/26366
 
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/include"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/include/wasmtime"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/linux-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/linux-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/macos-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/macos-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v23/build/windows-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/include"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/include/wasmtime"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/linux-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/linux-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/macos-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/macos-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v24/build/windows-x86_64"
 )
