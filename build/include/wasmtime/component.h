4c3a21bcc52e898d518c867b0c3563d6fafdf3e5
Alex Crichton <alex@alexcrichton.com>
2025-05-20 09:02:50 -0700

    v33.0.0 release artifacts

---
+++ build/include/wasmtime/component.h
@@ -0,0 +1,9 @@
+#ifndef WASMTIME_COMPONENT_H
+#define WASMTIME_COMPONENT_H
+
+#include <wasmtime/component/component.h>
+#include <wasmtime/component/func.h>
+#include <wasmtime/component/instance.h>
+#include <wasmtime/component/linker.h>
+
+#endif // WASMTIME_COMPONENT_H
