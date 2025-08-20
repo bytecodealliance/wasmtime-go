d21f75a7e1a6a9ed4b2e211d21a39cf3609682c0
Alex Crichton <alex@alexcrichton.com>
2025-08-20 10:16:43 -0700

    Bump to 36.0.0

--- example_test.go
+++ example_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bytecodealliance/wasmtime-go/v35"
+	"github.com/bytecodealliance/wasmtime-go/v36"
 )
 
 // Example of limiting a WebAssembly function's runtime using "fuel consumption".
