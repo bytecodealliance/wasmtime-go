2f783f08c1b9c015eb45e0f601289f501e6b93ab
Alex Crichton <alex@alexcrichton.com>
2025-09-29 16:18:04 -0500

    Bump to 37.0.0 (#252)

--- includebuild.go
+++ includebuild.go
@@ -12,11 +12,11 @@ import (
 	// included in vendored dependencies.
 	// Cf. https://github.com/golang/go/issues/26366
 
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/include"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/include/wasmtime"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/linux-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/linux-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/macos-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/macos-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v36/build/windows-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/include"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/include/wasmtime"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/linux-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/linux-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/macos-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/macos-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v37/build/windows-x86_64"
 )
