a0babc79a87a39428cd2f05338de8c5d81729bb7
Alex Crichton <alex@alexcrichton.com>
2025-12-22 08:16:27 -0800

    Bump to 40.0.0

--- includebuild.go
+++ includebuild.go
@@ -12,14 +12,14 @@ import (
 	// included in vendored dependencies.
 	// Cf. https://github.com/golang/go/issues/26366
 
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/include"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/include/wasmtime"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/include/wasmtime/component"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/include/wasmtime/component/types"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/include/wasmtime/types"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/linux-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/linux-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/macos-aarch64"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/macos-x86_64"
-	_ "github.com/bytecodealliance/wasmtime-go/v39/build/windows-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/include"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/include/wasmtime"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/include/wasmtime/component"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/include/wasmtime/component/types"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/include/wasmtime/types"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/linux-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/linux-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/macos-aarch64"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/macos-x86_64"
+	_ "github.com/bytecodealliance/wasmtime-go/v40/build/windows-x86_64"
 )
